f80995abab13457d1b829f4a22db1a63be1770bb
shiopon01 <shio.0323@gmail.com>
2025-07-09 01:27:36 +0900

    update

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -89,6 +89,12 @@
 ;; Ctrl-h をファイル削除にバインド
 (global-set-key (kbd "C-h") 'delete-backward-char)
 
+;; ミニバッファでもC-hを文字削除に設定
+(define-key minibuffer-local-map (kbd "C-h") 'delete-backward-char)
+(define-key minibuffer-local-ns-map (kbd "C-h") 'delete-backward-char)
+(define-key minibuffer-local-completion-map (kbd "C-h") 'delete-backward-char)
+(define-key minibuffer-local-must-match-map (kbd "C-h") 'delete-backward-char)
+
 ;; C-x C-b でバッファリストを ibuffer で開く
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
@@ -153,14 +159,113 @@
   (setq company-idle-delay 0.3)
   (setq company-minimum-prefix-length 2))
 
-;; helm: 検索・選択インターフェース
-(use-package helm
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x b" . helm-buffers-list)
-         ("C-x C-r" . helm-recentf))
+;; vertico: 垂直型補完UI
+(use-package vertico
   :config
-  (helm-mode 1))
+  (vertico-mode)
+  ;; 最大表示候補数を設定
+  (setq vertico-count 20)
+  ;; 最初の候補を選択状態にする
+  (setq vertico-preselect 'directory))
+
+;; orderless: 柔軟な検索パターン
+(use-package orderless
+  :config
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion))))
+  ;; 日本語でも使いやすいように設定
+  (setq orderless-matching-styles
+        '(orderless-literal orderless-regexp orderless-flex)))
+
+;; marginalia: 補完候補の詳細情報表示
+(use-package marginalia
+  :config
+  (marginalia-mode)
+  ;; 日本語環境でも適切に表示されるように設定
+  (setq marginalia-max-relative-age 0)
+  (setq marginalia-align 'right))
+
+;; consult: 追加コマンド
+(use-package consult
+  :bind (("C-x b" . consult-buffer)
+         ("C-x C-r" . consult-recent-file)
+         ("C-s" . consult-line)
+         ("M-g g" . consult-goto-line)
+         ("M-g M-g" . consult-goto-line)
+         ("M-y" . consult-yank-pop)
+         ("C-c h" . consult-history)
+         ("C-c m" . consult-mode-command)
+         ("C-c k" . consult-kmacro)
+         ("C-x M-:" . consult-complex-command)
+         ("C-x 4 b" . consult-buffer-other-window)
+         ("C-x 5 b" . consult-buffer-other-frame)
+         ("C-x r b" . consult-bookmark)
+         ("C-x p b" . consult-project-buffer)
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)
+         ("C-M-#" . consult-register)
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)
+         ("M-g o" . consult-outline)
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ("M-s d" . consult-find)
+         ("M-s D" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)
+         ("M-s e" . consult-isearch-history)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         :map minibuffer-local-map
+         ("M-s" . consult-history)
+         ("M-r" . consult-history))
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+  :init
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+  :config
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   :preview-key '(:debounce 0.4 any))
+  (setq consult-narrow-key "<"))
+
+;; embark: 選択項目に対するアクション
+(use-package embark
+  :bind
+  (("C-." . embark-act)
+   ("C-;" . embark-dwim)
+   ("C-h B" . embark-bindings))
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; embark-consult: embarkとconsultの統合
+(use-package embark-consult
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
 
 ;; typescript-mode
 (use-package typescript-mode
